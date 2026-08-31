379cc371d885aaf2c99af20e1efcc6e5147a5d85
illhammf <illhammf@users.noreply.github.com>
2026-08-31 10:57:45 +0700

    update 02_output.cs

---
+++ pertemuan-01/02_output.cs
@@ -0,0 +1,8 @@
+// Menampilkan beberapa baris teks
+// Console.WriteLine() digunakan untuk menampilkan teks
+// dan otomatis berpindah ke baris berikutnya.
+
+Console.WriteLine("Halo, semuanya!");
+Console.WriteLine("Nama saya Ilham.");
+Console.WriteLine("Saya sedang belajar bahasa C#.");
+Console.WriteLine("Semangat belajar!");
