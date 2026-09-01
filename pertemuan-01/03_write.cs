2ee11f459f12cd43f1f6c925add9bde6fde1c611
illhammf <illhammf@users.noreply.github.com>
2026-09-01 11:57:29 +0700

    update 03_write.cs

---
+++ pertemuan-01/03_write.cs
@@ -0,0 +1,8 @@
+// Console.Write() digunakan untuk menampilkan teks
+// tanpa berpindah ke baris berikutnya.
+
+Console.Write("Halo ");
+Console.Write("saya ");
+Console.Write("sedang ");
+Console.Write("belajar ");
+Console.Write("C#!");
