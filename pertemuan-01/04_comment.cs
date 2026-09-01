ce88b8d828009dda3dd19ef71bbd2c3b79c05f3e
illhammf <illhammf@users.noreply.github.com>
2026-09-01 11:59:16 +0700

    update 04_comment.cs

---
+++ pertemuan-01/04_comment.cs
@@ -0,0 +1,24 @@
+// ========================================
+// Contoh penggunaan komentar dalam C#
+// ========================================
+
+// Komentar satu baris
+// Baris ini tidak akan dijalankan oleh program.
+
+Console.WriteLine("Belajar C#");
+
+// Komentar dapat digunakan untuk menjelaskan kode.
+// Contohnya:
+// Menampilkan nama pengguna.
+Console.WriteLine("Nama: Ilham");
+
+/*
+    Ini adalah komentar
+    yang terdiri dari
+    beberapa baris.
+
+    Komentar seperti ini juga
+    tidak akan dijalankan oleh program.
+*/
+
+Console.WriteLine("Semangat belajar!");
