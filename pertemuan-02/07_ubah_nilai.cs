f36e9f12e7f8777d88a8448994dc4c83583c211b
illhammf <illhammf@users.noreply.github.com>
2026-09-02 09:39:32 +0700

    update 07_ubah_nilai.cs

---
+++ pertemuan-02/07_ubah_nilai.cs
@@ -0,0 +1,12 @@
+// Nilai variable dapat diubah setelah dibuat.
+
+int umur = 20;
+
+// Menampilkan nilai awal
+Console.WriteLine($"Umur awal : {umur}");
+
+// Mengubah nilai variable
+umur = 21;
+
+// Menampilkan nilai setelah diubah
+Console.WriteLine($"Umur baru : {umur}");
