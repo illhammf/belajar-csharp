6797a26d5ce4255c54edbc0b5209cdb875d7e709
illhammf <illhammf@users.noreply.github.com>
2026-09-02 09:40:31 +0700

    update 08_input.cs

---
+++ pertemuan-02/08_input.cs
@@ -0,0 +1,11 @@
+// Console.ReadLine() digunakan untuk menerima
+// input dari pengguna.
+
+// Meminta pengguna memasukkan nama
+Console.Write("Masukkan nama: ");
+
+// Membaca input dan menyimpannya ke variable
+string nama = Console.ReadLine();
+
+// Menampilkan kembali nama
+Console.WriteLine($"Halo, {nama}!");
