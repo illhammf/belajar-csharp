64cc256af65924231f79c7ed3e338058b45ab3b3
illhammf <illhammf@users.noreply.github.com>
2026-09-02 09:36:41 +0700

    update 04_integer.cs

---
+++ pertemuan-02/04_integer.cs
@@ -0,0 +1,14 @@
+// int digunakan untuk menyimpan bilangan bulat.
+
+int umur = 20;
+int nilai = 90;
+int jumlah = 10;
+
+// Melakukan perhitungan menggunakan variable
+int hasil = nilai + jumlah;
+
+// Menampilkan data
+Console.WriteLine($"Umur   : {umur}");
+Console.WriteLine($"Nilai  : {nilai}");
+Console.WriteLine($"Jumlah : {jumlah}");
+Console.WriteLine($"Hasil  : {hasil}");
