25abca7ff3fb05f06ef684419601041f694f1a8e
illhammf <illhammf@users.noreply.github.com>
2026-09-02 12:22:58 +0700

    update 03_comparison_operator.cs

---
+++ pertemuan-03/03_comparison_operator.cs
@@ -0,0 +1,30 @@
+// ==========================================
+// Comparison Operator
+// ==========================================
+
+int angkaA = 10;
+int angkaB = 5;
+
+// ==
+// Mengecek apakah kedua nilai sama
+Console.WriteLine($"A == B : {angkaA == angkaB}");
+
+// !=
+// Mengecek apakah kedua nilai berbeda
+Console.WriteLine($"A != B : {angkaA != angkaB}");
+
+// >
+// Mengecek apakah A lebih besar dari B
+Console.WriteLine($"A > B  : {angkaA > angkaB}");
+
+// <
+// Mengecek apakah A lebih kecil dari B
+Console.WriteLine($"A < B  : {angkaA < angkaB}");
+
+// >=
+// Mengecek apakah A lebih besar atau sama dengan B
+Console.WriteLine($"A >= B : {angkaA >= angkaB}");
+
+// <=
+// Mengecek apakah A lebih kecil atau sama dengan B
+Console.WriteLine($"A <= B : {angkaA <= angkaB}");
