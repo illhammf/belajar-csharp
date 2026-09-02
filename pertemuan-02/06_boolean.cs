6fb10baef8fb2ba7e11a92e1fb2167bc34891320
illhammf <illhammf@users.noreply.github.com>
2026-09-02 09:38:49 +0700

    update 06_boolean.cs

---
+++ pertemuan-02/06_boolean.cs
@@ -0,0 +1,9 @@
+// bool hanya memiliki dua kemungkinan nilai:
+// true atau false.
+
+bool sudahBelajar = true;
+bool sudahLulus = false;
+
+// Menampilkan nilai boolean
+Console.WriteLine($"Sudah belajar : {sudahBelajar}");
+Console.WriteLine($"Sudah lulus   : {sudahLulus}");
