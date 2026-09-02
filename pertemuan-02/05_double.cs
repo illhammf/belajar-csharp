4b79c025e617b3ea8a2cfcddd24bd68696a0a3e1
illhammf <illhammf@users.noreply.github.com>
2026-09-02 09:37:40 +0700

    update 05_double.cs

---
+++ pertemuan-02/05_double.cs
@@ -0,0 +1,10 @@
+// double digunakan untuk menyimpan angka desimal.
+
+double tinggi = 170.5;
+double berat = 60.5;
+double harga = 15000.50;
+
+// Menampilkan data
+Console.WriteLine($"Tinggi : {tinggi}");
+Console.WriteLine($"Berat  : {berat}");
+Console.WriteLine($"Harga  : {harga}");
