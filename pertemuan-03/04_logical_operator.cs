8f5dab5278729187aec7331a5a5fb008b323966d
illhammf <illhammf@users.noreply.github.com>
2026-09-02 12:29:04 +0700

    update 04_logical_operator.cs

---
+++ pertemuan-03/04_logical_operator.cs
@@ -0,0 +1,39 @@
+// ==========================================
+// Logical Operator
+// ==========================================
+
+bool memilikiTiket = true;
+bool sudahTerdaftar = true;
+
+// ==========================================
+// AND (&&)
+// ==========================================
+
+// Kedua kondisi harus true
+bool bolehMasuk = memilikiTiket && sudahTerdaftar;
+
+Console.WriteLine($"Boleh masuk : {bolehMasuk}");
+
+// ==========================================
+// OR (||)
+// ==========================================
+
+bool memilikiKartu = false;
+bool memilikiUndangan = true;
+
+// Salah satu kondisi true sudah cukup
+bool bolehMasuk2 = memilikiKartu || memilikiUndangan;
+
+Console.WriteLine($"Boleh masuk 2 : {bolehMasuk2}");
+
+// ==========================================
+// NOT (!)
+// ==========================================
+
+bool aktif = true;
+
+// ! digunakan untuk membalik nilai boolean
+bool tidakAktif = !aktif;
+
+Console.WriteLine($"Aktif      : {aktif}");
+Console.WriteLine($"Tidak aktif: {tidakAktif}");
