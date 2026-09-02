8a9711b582f93045914ecb55c06cc82c7ef7326c
illhammf <illhammf@users.noreply.github.com>
2026-09-02 12:35:51 +0700

    update 05_increment_decrement.cs

---
+++ pertemuan-03/05_increment_decrement.cs
@@ -0,0 +1,27 @@
+// ==========================================
+// Increment & Decrement
+// ==========================================
+
+int angka = 10;
+
+Console.WriteLine($"Nilai awal : {angka}");
+
+// ++ menambahkan nilai sebesar 1
+angka++;
+
+Console.WriteLine($"Setelah ++ : {angka}");
+
+// ++ lagi
+angka++;
+
+Console.WriteLine($"Setelah ++ : {angka}");
+
+// -- mengurangi nilai sebesar 1
+angka--;
+
+Console.WriteLine($"Setelah -- : {angka}");
+
+// -- lagi
+angka--;
+
+Console.WriteLine($"Setelah -- : {angka}");
