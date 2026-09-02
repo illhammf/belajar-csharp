7568c23ae0bce61579db389639c2283dd213a522
illhammf <illhammf@users.noreply.github.com>
2026-09-02 09:42:26 +0700

    update 10_input_double.cs

---
+++ pertemuan-02/10_input_double.cs
@@ -0,0 +1,9 @@
+// Meminta pengguna memasukkan angka desimal.
+
+Console.Write("Masukkan tinggi badan: ");
+
+// Mengubah input menjadi double
+double tinggi = double.Parse(Console.ReadLine());
+
+// Menampilkan hasil
+Console.WriteLine($"Tinggi badan kamu: {tinggi} cm");
