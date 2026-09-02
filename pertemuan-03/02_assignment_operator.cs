d9c51b283fdfb2ef3c6f69ff0ff1d443b02e32eb
illhammf <illhammf@users.noreply.github.com>
2026-09-02 11:38:05 +0700

    update 02_assignment_operator.cs

---
+++ pertemuan-03/02_assignment_operator.cs
@@ -0,0 +1,43 @@
+// ==========================================
+// Assignment Operator
+// ==========================================
+
+// Assignment dasar menggunakan =
+int angka = 10;
+
+Console.WriteLine($"Nilai awal : {angka}");
+
+// +=
+// Sama seperti:
+// angka = angka + 5;
+angka += 5;
+
+Console.WriteLine($"Setelah += 5 : {angka}");
+
+// -=
+// Sama seperti:
+// angka = angka - 3;
+angka -= 3;
+
+Console.WriteLine($"Setelah -= 3 : {angka}");
+
+// *=
+// Sama seperti:
+// angka = angka * 2;
+angka *= 2;
+
+Console.WriteLine($"Setelah *= 2 : {angka}");
+
+// /=
+// Sama seperti:
+// angka = angka / 4;
+angka /= 4;
+
+Console.WriteLine($"Setelah /= 4 : {angka}");
+
+// %=
+// Sama seperti:
+// angka = angka % 3;
+angka %= 3;
+
+Console.WriteLine($"Setelah %= 3 : {angka}");
