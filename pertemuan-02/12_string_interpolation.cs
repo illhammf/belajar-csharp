9da7796ad7c359c1f0a0244822817a36c7495fe2
illhammf <illhammf@users.noreply.github.com>
2026-09-02 09:44:50 +0700

    update 12_string_interpolation.cs

---
+++ pertemuan-02/12_string_interpolation.cs
@@ -0,0 +1,11 @@
+// String interpolation menggunakan tanda $
+// untuk memasukkan variable langsung ke dalam string.
+
+string nama = "Ilham";
+int umur = 20;
+string kota = "Tangerang";
+
+// Menampilkan beberapa variable menggunakan interpolation
+Console.WriteLine($"Nama : {nama}");
+Console.WriteLine($"Umur : {umur}");
+Console.WriteLine($"Kota : {kota}");
