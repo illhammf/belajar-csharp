abc0ba23eb05163f8a2eef1eacd4435eb2950794
illhammf <illhammf@users.noreply.github.com>
2026-09-02 11:02:28 +0700

    update 01_arithmetic_operator.cs

---
+++ pertemuan-03/01_arithmetic_operator.cs
@@ -0,0 +1,22 @@
+// ==========================================
+// Arithmetic Operator
+// ==========================================
+
+// Dua variabel untuk melakukan operasi matematika
+int angkaA = 20;
+int angkaB = 6;
+
+// Penjumlahan
+Console.WriteLine($"Penjumlahan : {angkaA + angkaB}");
+
+// Pengurangan
+Console.WriteLine($"Pengurangan : {angkaA - angkaB}");
+
+// Perkalian
+Console.WriteLine($"Perkalian   : {angkaA * angkaB}");
+
+// Pembagian
+Console.WriteLine($"Pembagian   : {angkaA / angkaB}");
+
+// Modulus digunakan untuk mendapatkan sisa pembagian
+Console.WriteLine($"Modulus     : {angkaA % angkaB}");
