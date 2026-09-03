bab488917f3cab57b759e2c91419d4fa01a141bd
illhammf <illhammf@users.noreply.github.com>
2026-09-03 13:18:03 +0700

    update 07_expression.cs

---
+++ pertemuan-03/07_expression.cs
@@ -0,0 +1,24 @@
+// ==========================================
+// Expression
+// ==========================================
+
+int angkaA = 10;
+int angkaB = 5;
+
+// Expression:
+// angkaA + angkaB
+int hasilTambah = angkaA + angkaB;
+
+Console.WriteLine($"Hasil tambah : {hasilTambah}");
+
+
+// Expression dengan beberapa operator
+int hasil = (angkaA + angkaB) * 2;
+
+Console.WriteLine($"Hasil akhir  : {hasil}");
+
+
+// Expression juga bisa digunakan dalam kondisi
+bool lebihBesar = angkaA > angkaB;
+
+Console.WriteLine($"A lebih besar dari B : {lebihBesar}");
