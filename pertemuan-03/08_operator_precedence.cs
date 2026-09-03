1db4ec3b484d7b95fe925ed942d0e88a89c47e30
illhammf <illhammf@users.noreply.github.com>
2026-09-03 13:21:30 +0700

    update 08_operator_precedence.cs

---
+++ pertemuan-03/08_operator_precedence.cs
@@ -0,0 +1,34 @@
+// ==========================================
+// Operator Precedence
+// ==========================================
+
+// Perkalian memiliki prioritas lebih tinggi
+// daripada penjumlahan.
+
+int hasil1 = 10 + 5 * 2;
+
+Console.WriteLine($"Hasil 1 : {hasil1}");
+
+
+// ==========================================
+// Menggunakan tanda kurung
+// ==========================================
+
+// Tanda kurung memiliki prioritas lebih tinggi
+// sehingga penjumlahan dilakukan terlebih dahulu.
+
+int hasil2 = (10 + 5) * 2;
+
+Console.WriteLine($"Hasil 2 : {hasil2}");
+
+
+// Contoh lainnya
+int hasil3 = 20 - 10 / 2;
+
+Console.WriteLine($"Hasil 3 : {hasil3}");
+
+
+// Dengan tanda kurung
+int hasil4 = (20 - 10) / 2;
+
+Console.WriteLine($"Hasil 4 : {hasil4}");
