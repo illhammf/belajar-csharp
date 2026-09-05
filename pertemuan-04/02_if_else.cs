afd8c6ad2058592029189cd6efb72f801e292f89
illhammf <illhammf@users.noreply.github.com>
2026-09-05 20:50:27 +0700

    update 02_if_else.cs

---
+++ pertemuan-04/02_if_else.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int nilai = 60;
+
+        // Jika nilai >= 75, maka lulus
+        if (nilai >= 75)
+        {
+            Console.WriteLine("Lulus");
+        }
+        // Jika kondisi if salah, maka else dijalankan
+        else
+        {
+            Console.WriteLine("Tidak lulus");
+        }
+    }
+}
