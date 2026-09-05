161971f9b5eae46bc20d8c9805a4985a3b6f1321
illhammf <illhammf@users.noreply.github.com>
2026-09-05 21:36:01 +0700

    update 01_while.cs

---
+++ pertemuan-05/01_while.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int i = 1;
+
+        // while akan berjalan selama kondisi bernilai true
+        while (i <= 5)
+        {
+            Console.WriteLine(i);
+
+            // Menambah nilai i agar looping tidak berjalan terus-menerus
+            i++;
+        }
+    }
+}
