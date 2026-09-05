5d257bff5c7a25f73350c1d4b3b42d5971cd4ece
illhammf <illhammf@users.noreply.github.com>
2026-09-05 20:48:41 +0700

    update 01_if.cs

---
+++ pertemuan-04/01_if.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int nilai = 80;
+
+        // Mengecek apakah nilai lebih besar atau sama dengan 75
+        if (nilai >= 75)
+        {
+            Console.WriteLine("Kamu lulus!");
+        }
+    }
+}
