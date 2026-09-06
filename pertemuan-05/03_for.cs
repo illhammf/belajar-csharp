27b716d0f59d0181a2c708edd2fd7e462d62aa06
illhammf <illhammf@users.noreply.github.com>
2026-09-06 07:57:12 +0700

    update 03_for.cs

---
+++ pertemuan-05/03_for.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Perulangan dimulai dari 1 sampai 5
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
