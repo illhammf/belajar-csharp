163003d8486d627e86de7c4da1a3f62913c0ca8e
illhammf <illhammf@users.noreply.github.com>
2026-09-07 09:47:02 +0700

    update 01_function.cs

---
+++ pertemuan-06/01_function.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    // Membuat method sederhana tanpa parameter
+    static void Sapa()
+    {
+        Console.WriteLine("Halo, selamat belajar C#!");
+    }
+
+    static void Main()
+    {
+        // Memanggil method Sapa
+        Sapa();
+    }
+}
