364ab3cd02f6e3e3222aad207097cdb5526a363f
illhammf <illhammf@users.noreply.github.com>
2026-09-08 08:05:43 +0700

    update 01_array.cs

---
+++ pertemuan-07/01_array.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Membuat array yang berisi beberapa angka
+        int[] angka = { 10, 20, 30, 40, 50 };
+
+        // Menampilkan seluruh isi array secara langsung
+        Console.WriteLine(angka[0]);
+        Console.WriteLine(angka[1]);
+        Console.WriteLine(angka[2]);
+        Console.WriteLine(angka[3]);
+        Console.WriteLine(angka[4]);
+    }
+}
