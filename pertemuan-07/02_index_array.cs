4bbf828e4900081b38295a0de3d6ed49e8de7af5
illhammf <illhammf@users.noreply.github.com>
2026-09-08 08:08:29 +0700

    update 02_index_array.cs

---
+++ pertemuan-07/02_index_array.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] angka = { 10, 20, 30, 40, 50 };
+
+        // Index array dimulai dari 0
+        Console.WriteLine($"Index 0: {angka[0]}");
+        Console.WriteLine($"Index 1: {angka[1]}");
+        Console.WriteLine($"Index 2: {angka[2]}");
+        Console.WriteLine($"Index 3: {angka[3]}");
+        Console.WriteLine($"Index 4: {angka[4]}");
+    }
+}
