79d8a8a8fa129c44e1a991a1816e507b782a5b9a
illhammf <illhammf@users.noreply.github.com>
2026-09-09 12:21:08 +0700

    update 04_string_char.cs

---
+++ pertemuan-08/04_string_char.cs
@@ -0,0 +1,8 @@
+// Membuat string
+string nama = "Ilham";
+
+// Mengakses karakter berdasarkan index
+// Index dimulai dari 0
+Console.WriteLine("Karakter pertama: " + nama[0]);
+Console.WriteLine("Karakter kedua: " + nama[1]);
+Console.WriteLine("Karakter terakhir: " + nama[nama.Length - 1]);
