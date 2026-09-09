98215324323f9a73465f633eea7ba7676bfc40bb
illhammf <illhammf@users.noreply.github.com>
2026-09-09 12:16:58 +0700

    update 03_string_length.cs

---
+++ pertemuan-08/03_string_length.cs
@@ -0,0 +1,9 @@
+// Membuat string
+string nama = "Ilham";
+
+// Length digunakan untuk mengetahui jumlah karakter
+int panjang = nama.Length;
+
+// Menampilkan jumlah karakter
+Console.WriteLine("Nama: " + nama);
+Console.WriteLine("Jumlah karakter: " + panjang);
