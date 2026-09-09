736871819e5492b6c15caf59d424ebaeefae178a
illhammf <illhammf@users.noreply.github.com>
2026-09-09 12:34:12 +0700

    update 11_string_replace.cs

---
+++ pertemuan-08/11_string_replace.cs
@@ -0,0 +1,9 @@
+// Membuat string
+string kalimat = "Saya belajar Java";
+
+// Mengganti kata Java menjadi C#
+string hasil = kalimat.Replace("Java", "C#");
+
+// Menampilkan hasil
+Console.WriteLine("Sebelum: " + kalimat);
+Console.WriteLine("Sesudah: " + hasil);
