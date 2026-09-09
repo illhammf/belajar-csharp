7c14158f29e1b6dc15c7f545fa2e2a1bd83a1be8
illhammf <illhammf@users.noreply.github.com>
2026-09-09 12:14:34 +0700

    update 02_input_string.cs

---
+++ pertemuan-08/02_input_string.cs
@@ -0,0 +1,8 @@
+// Meminta input nama dari pengguna
+Console.Write("Masukkan nama: ");
+
+// Membaca input dari keyboard
+string nama = Console.ReadLine() ?? "";
+
+// Menampilkan nama
+Console.WriteLine("Halo, " + nama);
