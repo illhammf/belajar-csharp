86fb21282c4792a80d5401ae332e5b486d64546f
illhammf <illhammf@users.noreply.github.com>
2026-09-09 12:35:19 +0700

    update 12_string_substring.cs

---
+++ pertemuan-08/12_string_substring.cs
@@ -0,0 +1,10 @@
+// Membuat string
+string nama = "Ilham";
+
+// Mengambil sebagian karakter
+// Mulai dari index 0 sebanyak 3 karakter
+string bagian = nama.Substring(0, 3);
+
+// Menampilkan hasil
+Console.WriteLine("Nama: " + nama);
+Console.WriteLine("Bagian: " + bagian);
