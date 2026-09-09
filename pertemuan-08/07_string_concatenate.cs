779e433411720c6b76cc2f1c7d6c4384dd83fef6
illhammf <illhammf@users.noreply.github.com>
2026-09-09 12:25:53 +0700

    update 07_string_concatenate.cs

---
+++ pertemuan-08/07_string_concatenate.cs
@@ -0,0 +1,12 @@
+// Membuat beberapa string
+string namaDepan = "Ilham";
+string namaBelakang = "Firmansyah";
+
+// Menggabungkan string menggunakan +
+string namaLengkap = namaDepan + " " + namaBelakang;
+
+// Menampilkan hasil
+Console.WriteLine("Nama lengkap: " + namaLengkap);
+
+// Menggabungkan string menggunakan interpolation
+Console.WriteLine($"Halo, {namaDepan} {namaBelakang}!");
