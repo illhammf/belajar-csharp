c613dd1fada050e484ab0912344f9d55fd3f09af
illhammf <illhammf@users.noreply.github.com>
2026-09-09 12:22:57 +0700

    update 05_string_loop.cs

---
+++ pertemuan-08/05_string_loop.cs
@@ -0,0 +1,9 @@
+// Membuat string
+string nama = "Ilham";
+
+// Melakukan perulangan untuk setiap karakter
+for (int i = 0; i < nama.Length; i++)
+{
+    // Menampilkan karakter satu per satu
+    Console.WriteLine("Index " + i + ": " + nama[i]);
+}
