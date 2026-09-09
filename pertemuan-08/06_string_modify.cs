7b155fd040d7f47218868e8f355c42e2d52050ba
illhammf <illhammf@users.noreply.github.com>
2026-09-09 12:24:03 +0700

    update 06_string_modify.cs

---
+++ pertemuan-08/06_string_modify.cs
@@ -0,0 +1,13 @@
+// Membuat string
+string nama = "Ilham Firmansyah";
+
+// Mengubah semua huruf menjadi huruf besar
+string besar = nama.ToUpper();
+
+// Mengubah semua huruf menjadi huruf kecil
+string kecil = nama.ToLower();
+
+// Menampilkan hasil
+Console.WriteLine("Asli  : " + nama);
+Console.WriteLine("Besar : " + besar);
+Console.WriteLine("Kecil : " + kecil);
