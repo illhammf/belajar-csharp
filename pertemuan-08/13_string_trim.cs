9e7783be25ec0c253dcd6cdedf9cde20e8dd143a
illhammf <illhammf@users.noreply.github.com>
2026-09-09 12:38:01 +0700

    update 13_string_trim.cs

---
+++ pertemuan-08/13_string_trim.cs
@@ -0,0 +1,9 @@
+// String memiliki spasi di awal dan akhir
+string nama = "   Ilham   ";
+
+// Trim menghapus spasi di awal dan akhir
+string hasil = nama.Trim();
+
+// Menampilkan hasil
+Console.WriteLine("Sebelum: [" + nama + "]");
+Console.WriteLine("Sesudah: [" + hasil + "]");
