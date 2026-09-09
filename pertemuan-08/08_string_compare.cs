d7c5fd118771d284c415420e9adaf1a4bff8c861
illhammf <illhammf@users.noreply.github.com>
2026-09-09 12:27:53 +0700

    update 08_string_compare.cs

---
+++ pertemuan-08/08_string_compare.cs
@@ -0,0 +1,19 @@
+// Membuat dua string
+string username1 = "ilham";
+string username2 = "ilham";
+
+// Membandingkan dua string
+if (username1 == username2)
+{
+    Console.WriteLine("Username sama");
+}
+else
+{
+    Console.WriteLine("Username berbeda");
+}
+
+// Membandingkan menggunakan !=
+if (username1 != "admin")
+{
+    Console.WriteLine("Username bukan admin");
+}
