cee3ed723a6db6a0cdfc948eca22688c4da44ce5
illhammf <illhammf@users.noreply.github.com>
2026-09-18 21:19:36 +0700

    update 07_list_count.cs

---
+++ pertemuan-15/07_list_count.cs
@@ -0,0 +1,14 @@
+// Pertemuan 15 - Collections
+// Menghitung jumlah data menggunakan Count
+
+List<int> angka = new List<int>
+{
+    10,
+    20,
+    30,
+    40,
+    50
+};
+
+// Count digunakan untuk mengetahui jumlah data
+Console.WriteLine($"Jumlah data: {angka.Count}");
