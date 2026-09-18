37fbfff7b63842d23649fefee6fe5ca6c2eb29be
illhammf <illhammf@users.noreply.github.com>
2026-09-18 21:05:59 +0700

    update 03_list_access.cs

---
+++ pertemuan-15/03_list_access.cs
@@ -0,0 +1,14 @@
+// Pertemuan 15 - Collections
+// Mengakses data List menggunakan index
+
+List<string> nama = new List<string>
+{
+    "Ilham",
+    "Budi",
+    "Andi"
+};
+
+// Index dimulai dari 0
+Console.WriteLine($"Index 0: {nama[0]}");
+Console.WriteLine($"Index 1: {nama[1]}");
+Console.WriteLine($"Index 2: {nama[2]}");
