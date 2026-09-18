d4eaeb7b93571cd5f4cfc26b8a86d2c2c706e269
illhammf <illhammf@users.noreply.github.com>
2026-09-18 21:04:29 +0700

    update 01_list.cs

---
+++ pertemuan-15/01_list.cs
@@ -0,0 +1,8 @@
+// Pertemuan 15 - Collections
+// Mengenal List<T>
+
+// Membuat List kosong yang menyimpan data int
+List<int> angka = new List<int>();
+
+// Menampilkan jumlah data
+Console.WriteLine($"Jumlah data: {angka.Count}");
